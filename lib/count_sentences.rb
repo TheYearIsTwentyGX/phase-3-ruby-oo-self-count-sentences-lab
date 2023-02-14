ef5e8ef4046e93fff23ac06b4b1656c3fc1e6615
Dylan Hays <twentygx@outlook.com>
2023-02-13 18:35:49 -0600

    commit tree 59db786c1d2bdc50192f9f6ca9b5f023f46aa717

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.?!]/)
+    sentence_array.delete_if {|sentence| sentence.empty?}
+    return sentence_array.length
   end
 end
